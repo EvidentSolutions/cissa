b357af67c9430d6fa8066a4f67d841c3a6d02473
Juha Komulainen <juha.komulainen@evident.fi>
2011-06-04 13:45:55 +0300

    Added Dimension.

---
+++ src/main/java/fi/evident/cissa/Dimension.java
@@ -0,0 +1,87 @@
+package fi.evident.cissa;
+
+import java.math.BigDecimal;
+
+public final class Dimension {
+
+    private final BigDecimal value;
+    private final DimensionUnit unit;
+
+    private Dimension(BigDecimal value, DimensionUnit unit) {
+        if (value == null) throw new IllegalArgumentException("null value");
+        if (unit == null) throw new IllegalArgumentException("null unit");
+        
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public DimensionUnit getUnit() {
+        return unit;
+    }
+
+    public static Dimension dimension(long value) {
+        return dimension(value, DimensionUnit.EMPTY);
+    }
+
+    public static Dimension dimension(long value, DimensionUnit unit) {
+        return dimension(new BigDecimal(value), unit);
+    }
+
+    public static Dimension dimension(String value) {
+        return dimension(value, DimensionUnit.EMPTY);
+    }
+
+    public static Dimension dimension(String value, DimensionUnit unit) {
+        return dimension(new BigDecimal(value), unit);
+    }
+
+    public static Dimension dimension(BigDecimal value) {
+        return dimension(value, DimensionUnit.EMPTY);
+    }
+
+    public static Dimension dimension(BigDecimal value, DimensionUnit unit) {
+        return new Dimension(value, unit);
+    }
+
+    public Dimension add(Dimension rhs) {
+        return dimension(value.add(rhs.value), unit.add(rhs.unit));
+    }
+
+    public Dimension subtract(Dimension rhs) {
+        return dimension(value.subtract(rhs.value), unit.subtract(rhs.unit));
+    }
+
+    public Dimension multiply(Dimension rhs) {
+        return dimension(value.multiply(rhs.value), unit.multiply(rhs.unit));
+    }
+
+    public Dimension divide(Dimension rhs) {
+        return dimension(value.divide(rhs.value), unit.divide(rhs.unit));
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o instanceof Dimension) {
+            Dimension rhs = (Dimension) o;
+            return value.equals(rhs.value);
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
---
+++ src/test/java/fi/evident/cissa/DimensionTest.java
@@ -0,0 +1,82 @@
+package fi.evident.cissa;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static fi.evident.cissa.Dimension.dimension;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.*;
+
+public class DimensionTest {
+
+    @Test
+    public void shouldHaveSensibleStringRepresentation() {
+        assertEquals("8", dimension(8).toString());
+        assertEquals("5", dimension("5").toString());
+        assertEquals("5.123", dimension("5.123").toString());
+        assertEquals("-1.2", dimension("-1.2").toString());
+    }
+
+    @Test(expected = Exception.class)
+    public void shouldNotBeConstructableFromNull() {
+        dimension((BigDecimal) null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotBeConstructableFromEmptyString() {
+        dimension("");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldNotBeConstructableFromStringNotRepresentingNumber() {
+        dimension("foo");
+    }
+
+    @Test
+    public void shouldDefineEquality() {
+        assertThat("equal with same value", dimension("8.424"), equalTo(dimension("8.424")));
+        assertThat("not equal with different value", dimension("8.4"), not(equalTo(dimension("4.5"))));
+        assertThat("not equal with null", dimension("123"), not(equalTo(null)));
+        assertFalse("not equal with other type", dimension("42").equals("foo"));
+    }
+
+    @Test
+    public void definesConsistentHashCode() {
+        assertEquals(dimension("5.4").hashCode(), dimension("5.4").hashCode());
+    }
+
+    @Test
+    public void shouldSupportBasicArithmetic() {
+        assertEquals(dimension("8.6"), dimension("6.1").add(dimension("2.5")));
+        assertEquals(dimension("3.6"), dimension("6.1").subtract(dimension("2.5")));
+        assertEquals(dimension("15.25"), dimension("6.1").multiply(dimension("2.5")));
+        assertEquals(dimension("2.44"), dimension("6.1").divide(dimension("2.5")));
+        assertEquals(dimension("2.5"), dimension("5").divide(dimension("2")));
+    }
+
+    @Test
+    public void byDefaultThereIsNoUnit() {
+        assertEquals(DimensionUnit.EMPTY, dimension("5.5").getUnit());
+    }
+
+    @Test
+    public void shouldSupportExtractingValueAndUnit() {
+        BigDecimal value = new BigDecimal("24.5");
+        DimensionUnit unit = DimensionUnit.forName("foo");
+
+        Dimension dim = dimension(value, unit);
+        assertEquals(value, dim.getValue());
+        assertEquals(unit, dim.getUnit());
+    }
+
+    @Test
+    public void shouldCalculateCorrectUnits() {
+        DimensionUnit m = DimensionUnit.forName("m");
+
+        assertEquals(dimension(10, m), dimension(5, m).add(dimension(5, m)));
+        assertEquals(dimension(10, m), dimension(5).add(dimension(5, m)));
+        assertEquals(dimension(10, m), dimension(5, m).add(dimension(5)));
+    }
+}
